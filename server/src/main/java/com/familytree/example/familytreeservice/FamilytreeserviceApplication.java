31435284e1cc146ce63f123ddf4fa19863040edc
M <mrgmckenzie@gmail.com>
2023-11-23 16:52:44 +0000

    first commit

---
+++ server/src/main/java/com/familytree/example/familytreeservice/FamilytreeserviceApplication.java
@@ -0,0 +1,13 @@
+package com.familytree.example.familytreeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilytreeserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilytreeserviceApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/familytree/example/familytreeservice/FamilytreeserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.familytree.example.familytreeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilytreeserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
