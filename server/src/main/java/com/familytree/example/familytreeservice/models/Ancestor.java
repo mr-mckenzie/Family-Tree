e5381f43d44db4e46a7236ee3a661ef784a5f106
M <mrgmckenzie@gmail.com>
2023-11-24 17:36:15 +0000

    created ancestor class and tests

--- server/src/main/java/com/familytree/example/familytreeservice/models/Ancestor.java
+++ server/src/main/java/com/familytree/example/familytreeservice/models/Ancestor.java
@@ -1,2 +1,31 @@
-package com.familytree.example.familytreeservice.models;public class Ancestor {
+package com.familytree.example.familytreeservice.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ancestor {
+
+    private String forename;
+    private String surname;
+    private Gender gender;
+    private ArrayList <Event> lifeEvents;
+
+    public Ancestor(String forename, String surname, Gender gender) {
+        this.forename = forename;
+        this.surname = surname;
+        this.gender = gender;
+        this.lifeEvents = new ArrayList <Event>();
+    }
+
+    public String getForename() {
+        return forename;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
 }
--- server/src/test/java/com/familytree/example/familytreeservice/AncestorTest.java
+++ server/src/test/java/com/familytree/example/familytreeservice/AncestorTest.java
@@ -1,2 +1,37 @@
-package com.familytree.example.familytreeservice;public class AncestorTest {
+package com.familytree.example.familytreeservice;
+
+import com.familytree.example.familytreeservice.models.Ancestor;
+import com.familytree.example.familytreeservice.models.Event;
+import com.familytree.example.familytreeservice.models.Gender;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+
+public class AncestorTest {
+
+    public Ancestor ancestor;
+
+    @BeforeEach
+    public void setUp() {
+        ancestor = new Ancestor("Robert", "Bruce", Gender.MALE);
+    }
+
+    @Test
+    public void hasForename(){
+        assertEquals("Robert", ancestor.getForename());
+    }
+
+    @Test
+    public void hasSurname(){
+        assertEquals("Bruce", ancestor.getSurname());
+    }
+
+    @Test
+    public void hasGender(){
+        assertEquals(Gender.MALE, ancestor.getGender());
+        assertEquals("Male", ancestor.getGender().getGender());
+    }
+
 }
