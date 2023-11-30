65c16edad3386bfbbe2de7a8b10d3f3acab28688
M <mrgmckenzie@gmail.com>
2023-11-30 11:16:16 +0000

    added custom sort by date

--- server/src/main/java/com/familytree/example/familytreeservice/models/Event/Event.java
+++ server/src/main/java/com/familytree/example/familytreeservice/models/Event/Event.java
@@ -2,7 +2,7 @@ package com.familytree.example.familytreeservice.models.Event;
 
 import java.util.Calendar;
 
-public abstract class Event {
+public abstract class Event implements Comparable {
     EventType type;
     private Calendar date;
     private String location;
@@ -28,5 +28,16 @@ public abstract class Event {
 
     public String getCitation() {
         return citation;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return ((Event) obj).getDate().equals(getDate());
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        Event e = (Event) o;
+        return getDate().compareTo(e.getDate());
     }
 }
