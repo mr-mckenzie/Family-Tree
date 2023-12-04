c9e3309351cf32bd8db5c2a83c088feb62246a63
M <mrgmckenzie@gmail.com>
2023-12-04 20:53:09 +0000

    added get all route

--- server/src/main/java/com/familytree/example/familytreeservice/controllers/AncestorController.java
+++ server/src/main/java/com/familytree/example/familytreeservice/controllers/AncestorController.java
@@ -1,12 +1,13 @@
 package com.familytree.example.familytreeservice.controllers;
 
 import com.familytree.example.familytreeservice.models.Ancestor;
-import com.familytree.example.familytreeservice.models.Gender;
 import com.familytree.example.familytreeservice.repositories.AncestorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class AncestorController {
@@ -14,11 +15,15 @@ public class AncestorController {
     @Autowired
     AncestorRepository ancestorRepository;
 
-    @PostMapping(value = "/ancestors", consumes="application/json", produces ="application/json")
+    @PostMapping(value = "/ancestors")
     public ResponseEntity<Ancestor> postAncestor(@RequestBody Ancestor ancestor) {
         ancestorRepository.save(ancestor);
         return new ResponseEntity<>(ancestor, HttpStatus.CREATED);
     }
 
+    @GetMapping(value = "/ancestors")
+    public ResponseEntity<List<Ancestor>> getAncestors() {
+        return new ResponseEntity<>(ancestorRepository.findAll(), HttpStatus.OK);
+    }
 
 }
