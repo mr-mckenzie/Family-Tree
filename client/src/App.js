3f7769c0fefc966be9a75d1d0e2bb3afc5eabee4
M <mrgmckenzie@gmail.com>
2023-12-05 10:05:36 +0000

    added mapping of ancestors list

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import { useState } from 'react';
-import { postAncestor } from './services/InternalServices';
+import { useEffect, useState } from 'react';
+import { getAllAncestors, postAncestor } from './services/InternalServices';
 
 function App() {
 
@@ -31,25 +31,42 @@ function App() {
     setGender("")
   }
 
+  useEffect( () => {
+    getAllAncestors().then((results) => {
+      setAncestors(results)
+    })
+  }, [])
+
+  const ancestorList = ancestors.map((ancestor) => {
+    return (
+        <p key={ancestor.id} value={ancestor.id}>{ancestor.forename} {ancestor.surname}, {ancestor.gender}</p>
+    );
+});
+
   return (
-    <div className="App">
-      <form onSubmit={handleSubmit}>
-        <label>Forename:
-          <input className="forename" type="text" onChange={handleFornameChange} value={forename} required/>
-        </label>
-        <label>Surname:
-          <input className="surname" type="text" onChange={handleSurnameChange} value={surname} required/>
-        </label>
-        <label>Gender:
-          <select className="gender" type="select" onChange={handleGenderChange} value={gender} required>
-            <option value="">Please select</option>
-            <option value="MALE">Male</option>
-            <option value="FEMALE">Female</option>
-          </select>
-        </label>
-        <input className="submit" type="submit" value="Submit"/>
-      </form>
-    </div>
+    <>
+      <div className="ancestorForm">
+        <form onSubmit={handleSubmit}>
+          <label>Forename:
+            <input className="forename" type="text" onChange={handleFornameChange} value={forename} required/>
+          </label>
+          <label>Surname:
+            <input className="surname" type="text" onChange={handleSurnameChange} value={surname} required/>
+          </label>
+          <label>Gender:
+            <select className="gender" type="select" onChange={handleGenderChange} value={gender} required>
+              <option value="">Please select</option>
+              <option value="MALE">Male</option>
+              <option value="FEMALE">Female</option>
+            </select>
+          </label>
+          <input className="submit" type="submit" value="Submit"/>
+        </form>
+      </div>
+      <div className="ancestorsList">
+        {ancestorList}
+      </div>
+    </>
   );
 }
 
