98b1e8a1614ef069aa5b82e924b28699b98bf3f4
M <mrgmckenzie@gmail.com>
2024-01-08 15:33:40 +0000

    added form for events and basic display of birth data

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { useEffect, useState } from 'react';
-import { getAllAncestors, postAncestor } from './services/InternalServices';
+import { getAllAncestors, getAllBirths, postAncestor, postEvent } from './services/InternalServices';
 
 function App() {
 
@@ -19,19 +19,19 @@ function App() {
     setGender(event.target.value)
   }
 
-  const handleSubmit = (event) => {
+  const handleAncestorSubmit = (event) => {
     event.preventDefault()
     postAncestor({
-      forename: forename, 
-      surname : surname, 
-      gender : gender
+      forename: forename,
+      surname: surname,
+      gender: gender
     })
     setForename("")
     setSurname("")
     setGender("")
   }
 
-  useEffect( () => {
+  useEffect(() => {
     getAllAncestors().then((results) => {
       setAncestors(results)
     })
@@ -39,19 +39,63 @@ function App() {
 
   const ancestorList = ancestors.map((ancestor) => {
     return (
-        <p key={ancestor.id} value={ancestor.id}>{ancestor.forename} {ancestor.surname}, {ancestor.gender}</p>
+      <p key={ancestor.id} value={ancestor.id}>{ancestor.forename} {ancestor.surname}, {ancestor.gender}</p>
     );
-});
+  });
+
+  const [eventType, setEventType] = useState("")
+  const [date, setDate] = useState("")
+  const [location, setLocation] = useState("")
+  const [citation, setCitation] = useState("")
+  const [births, setBirths] = useState([])
+
+  const handleEventTypeChange = (event) => {
+    setEventType(event.target.value)
+  }
+  const handleDateChange = (event) => {
+    setDate(event.target.value)
+  }
+  const handleLocationChange = (event) => {
+    setLocation(event.target.value)
+  }
+  const handleCitationChange = (event) => {
+    setCitation(event.target.value)
+  }
+
+  const handleEventSubmit = (event) => {
+    event.preventDefault()
+    postEvent({
+      type: eventType,
+      date: date,
+      location: location,
+      citation: citation
+    })
+    setEventType("")
+    setDate("")
+    setLocation("")
+    setCitation("")
+  }
+
+  useEffect(() => {
+    getAllBirths().then(results => { setBirths(results) })
+  }, [])
+
+  const birthsList = births.map((birth) => {
+    return (
+      <li key={birth.id} value={birth.id}>{birth.date} {birth.location}, {birth.citation}</li>
+    );
+  });
 
   return (
     <>
+      <h1>Ancestors</h1>
       <div className="ancestorForm">
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleAncestorSubmit}>
           <label>Forename:
-            <input className="forename" type="text" onChange={handleFornameChange} value={forename} required/>
+            <input className="forename" type="text" onChange={handleFornameChange} value={forename} required />
           </label>
           <label>Surname:
-            <input className="surname" type="text" onChange={handleSurnameChange} value={surname} required/>
+            <input className="surname" type="text" onChange={handleSurnameChange} value={surname} required />
           </label>
           <label>Gender:
             <select className="gender" type="select" onChange={handleGenderChange} value={gender} required>
@@ -60,12 +104,46 @@ function App() {
               <option value="FEMALE">Female</option>
             </select>
           </label>
-          <input className="submit" type="submit" value="Submit"/>
+          <input className="submit" type="submit" value="Submit" />
         </form>
       </div>
       <div className="ancestorsList">
+        <h2>Ancestors</h2>
         {ancestorList}
       </div>
+      <div>
+        <form className="eventForm" onSubmit={handleEventSubmit}>
+          <label>
+            Event type:
+            <select value={eventType} onChange={handleEventTypeChange} required>
+              <option value="">Please select</option>
+              <option value="BIRTH">Birth</option>
+              {/* <option value="MARRIAGE">Marriage</option> */}
+              {/* <option value="CENSUS">Census</option> */}
+              <option value="DEATH">Death</option>
+              {/* <option value="OTHER">Other</option> */}
+            </select>
+          </label>
+          <label>Date:
+            <input type="date" value={date} onChange={handleDateChange} required></input>
+          </label>
+          <label>
+            Location:
+            <input type="text" value={location} onChange={handleLocationChange} required />
+          </label>
+          <label>
+            Citation:
+            <input type="text" value={citation} onChange={handleCitationChange} required />
+          </label>
+          <input type="submit" />
+        </form>
+      </div>
+      <div className="birthsList">
+        <h2>Births</h2>
+        <ul>{birthsList}
+        </ul>
+      </div>
+
     </>
   );
 }
--- client/src/services/InternalServices.js
+++ client/src/services/InternalServices.js
@@ -16,3 +16,32 @@ export const getAllAncestors = () => {
     }).then(res => res.json())
 
 }
+
+export const postEvent = (payload) => {
+    let eventRoute = ""
+    if (payload.type == "BIRTH"){
+        eventRoute = "births"
+    } else if (payload.type == "DEATH") {
+        eventRoute = "deaths"
+    }
+    return fetch(baseUrl + eventRoute, {
+        method: "POST",
+        body: JSON.stringify(payload),
+        headers:{"Content-Type" : "application/json"}
+    }).then(res => res.json())
+}
+
+export const getAllBirths = () => {
+    return fetch(baseUrl + "births", {
+        method: "GET",
+        headers:{"Content-Type" : "application/json"}
+    }).then(res => res.json())
+}
+
+export const addBirth = (payload, ancestorId) => {
+    return fetch(baseUrl + "ancestors/" + ancestorId + "/birth", {
+        method: "PUT",
+        body: JSON.stringify(payload),
+        headers:{"Content-Type" : "application/json"}
+    }).then(res => res.json())
+}
